28fd37cdbc7c66f683037ffd6e3b2d7db1767154
BeckettPorter <porterbeckett@gmail.com>
2024-09-03 16:44:38 -0700

    Worked last night and tonight but couldn't commit and push yesterday because I had to go suddenly

--- WORK_LOG.md
+++ WORK_LOG.md
@@ -1,9 +1,10 @@
 Name: YOUR NAME HERE
 
-| Date    |  Time  |                                     Update |
-|:--------|:------:|-------------------------------------------:|
-| April 5 | 9-10pm | I tried to write abstract, efficient code. |
-|         |        |                                            |
+| Date          |     Time     |                                                                Update |
+|:--------------|:------------:|----------------------------------------------------------------------:|
+| April 5       |    9-10pm    |                            I tried to write abstract, efficient code. |
+| September 2nd |  5-5:30 pm   |                                    Started working on helper methods. |
+| September 3rd | 4:15-4:45 pm | Tried to fix bugs and get the code closer to working. (still doesn't) |
 
 
 To add a new row to the table, click into a cell and then hit shift-enter.
--- src/MokshaPatam.java
+++ src/MokshaPatam.java
@@ -4,7 +4,7 @@
  * for Adventures in Algorithms
  * at Menlo School in Atherton, CA
  *
- * Completed by: [YOUR NAME HERE]
+ * Completed by: Beckett Porter
  *
  */
 
@@ -16,6 +16,81 @@ public class MokshaPatam {
      */
     public static int fewestMoves(int boardsize, int[][] ladders, int[][] snakes) {
 
-        return 0;
+        int currentPos = 0;
+        int numMovesTaken = 0;
+
+        while(currentPos != boardsize)
+        {
+            if (getDistanceToClosestLadder(currentPos, ladders) != -1)
+            {
+                numMovesTaken += calcNumDiceRollsToReachLocation(currentPos,
+                        getDistanceToClosestLadder(currentPos, ladders));
+
+
+                int closestLadderPos = currentPos + getDistanceToClosestLadder(currentPos, ladders);
+
+                for (int i = 0; i < ladders.length; i++)
+                {
+                    if (ladders[i][0] == closestLadderPos)
+                    {
+                        currentPos += ladders[i][1];
+                    }
+                }
+            }
+            // Otherwise, this means that there is no ladder between the currentPos and end of board.
+            else
+            {
+                numMovesTaken += calcNumDiceRollsToReachLocation(currentPos, boardsize);
+                currentPos = boardsize;
+            }
+        }
+
+        return numMovesTaken;
+    }
+
+
+    // Method I made to calculate the minimum number of roles needed to reach a target location from a start point.
+    private static int calcNumDiceRollsToReachLocation(int startLocation, int targetLocation)
+    {
+        int currentIndex = startLocation;
+        int numRolls = 0;
+
+        while (currentIndex != targetLocation)
+        {
+            if ((targetLocation - currentIndex) >= 6)
+            {
+                currentIndex += 6;
+                numRolls++;
+            }
+            else
+            {
+                currentIndex += (targetLocation - currentIndex);
+                numRolls++;
+            }
+        }
+        return numRolls;
+    }
+
+    private static int getDistanceToClosestLadder(int startLocation, int[][] laddersAr)
+    {
+        int currentClosestLadderPos = laddersAr[0][0] - startLocation;
+        boolean foundLadder = false;
+
+        for (int i = 0; i < laddersAr.length; i++)
+        {
+            int ladderPos = laddersAr[i][0];
+            if (ladderPos > startLocation && ladderPos - startLocation < currentClosestLadderPos)
+            {
+                currentClosestLadderPos = ladderPos;
+                foundLadder = true;
+            }
+        }
+
+        if (foundLadder)
+        {
+            return currentClosestLadderPos - startLocation;
+        }
+        // Return -1 if can't find ladder (would mean they are near the end).
+        return -1;
     }
 }
