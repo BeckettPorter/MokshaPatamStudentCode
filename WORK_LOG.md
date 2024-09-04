46b1e0eaeee688bfb196c90ac1f992c60ee679f3
BeckettPorter <porterbeckett@gmail.com>
2024-09-04 11:55:02 -0700

    Refactoring code because I realized it won't work and I need to check for snakes.

--- WORK_LOG.md
+++ WORK_LOG.md
@@ -1,10 +1,11 @@
 Name: YOUR NAME HERE
 
-| Date          |     Time     |                                                                Update |
-|:--------------|:------------:|----------------------------------------------------------------------:|
-| April 5       |    9-10pm    |                            I tried to write abstract, efficient code. |
-| September 2nd |  5-5:30 pm   |                                    Started working on helper methods. |
-| September 3rd | 4:15-4:45 pm | Tried to fix bugs and get the code closer to working. (still doesn't) |
+| Date          |     Time      |                                                                            Update |
+|:--------------|:-------------:|----------------------------------------------------------------------------------:|
+| April 5       |    9-10pm     |                                        I tried to write abstract, efficient code. |
+| September 2nd |   5-5:30 pm   |                                                Started working on helper methods. |
+| September 3rd | 4:15-4:45 pm  |             Tried to fix bugs and get the code closer to working. (still doesn't) |
+| September 4th | 11:30 - 12:00 | Refactoring code because I realized it won't work and I need to check for snakes. |
 
 
 To add a new row to the table, click into a cell and then hit shift-enter.
--- src/MokshaPatam.java
+++ src/MokshaPatam.java
@@ -19,29 +19,11 @@ public class MokshaPatam {
         int currentPos = 0;
         int numMovesTaken = 0;
 
-        while(currentPos != boardsize)
+        while (currentPos != boardsize)
         {
             if (getDistanceToClosestLadder(currentPos, ladders) != -1)
             {
-                numMovesTaken += calcNumDiceRollsToReachLocation(currentPos,
-                        getDistanceToClosestLadder(currentPos, ladders));
 
-
-                int closestLadderPos = currentPos + getDistanceToClosestLadder(currentPos, ladders);
-
-                for (int i = 0; i < ladders.length; i++)
-                {
-                    if (ladders[i][0] == closestLadderPos)
-                    {
-                        currentPos += ladders[i][1];
-                    }
-                }
-            }
-            // Otherwise, this means that there is no ladder between the currentPos and end of board.
-            else
-            {
-                numMovesTaken += calcNumDiceRollsToReachLocation(currentPos, boardsize);
-                currentPos = boardsize;
             }
         }
 
@@ -49,31 +31,31 @@ public class MokshaPatam {
     }
 
 
-    // Method I made to calculate the minimum number of roles needed to reach a target location from a start point.
-    private static int calcNumDiceRollsToReachLocation(int startLocation, int targetLocation)
-    {
-        int currentIndex = startLocation;
-        int numRolls = 0;
-
-        while (currentIndex != targetLocation)
-        {
-            if ((targetLocation - currentIndex) >= 6)
-            {
-                currentIndex += 6;
-                numRolls++;
-            }
-            else
-            {
-                currentIndex += (targetLocation - currentIndex);
-                numRolls++;
-            }
-        }
-        return numRolls;
-    }
+//    // Method I made to calculate the minimum number of roles needed to reach a target location from a start point.
+//    private static int calcNumDiceRollsToReachLocation(int startLocation, int targetLocation)
+//    {
+//        int currentIndex = startLocation;
+//        int numRolls = 0;
+//
+//        while (currentIndex != targetLocation)
+//        {
+//            if ((targetLocation - currentIndex) >= 6)
+//            {
+//                currentIndex += 6;
+//                numRolls++;
+//            }
+//            else
+//            {
+//                currentIndex += (targetLocation - currentIndex);
+//                numRolls++;
+//            }
+//        }
+//        return numRolls;
+//    }
 
     private static int getDistanceToClosestLadder(int startLocation, int[][] laddersAr)
     {
-        int currentClosestLadderPos = laddersAr[0][0] - startLocation;
+        int currentClosestLadderPos = Integer.MAX_VALUE;
         boolean foundLadder = false;
 
         for (int i = 0; i < laddersAr.length; i++)
@@ -92,5 +74,16 @@ public class MokshaPatam {
         }
         // Return -1 if can't find ladder (would mean they are near the end).
         return -1;
+    }
+
+    private static boolean doesLocationContainSnake(int loc, int[][] snakesAr)
+    {
+        for (int i = 0; i < snakesAr.length; i++)
+        {
+            if (snakesAr[i][0] == loc) {
+                return true;
+            }
+        }
+        return false;
     }
 }
